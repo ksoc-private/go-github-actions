6a8192154d702654ebadfd865ac3524192f87bf0
Max Brain <max.brain@ksoc.com>
2023-12-15 17:15:50 +0800

    Updates image push script to build manifest and push images
    Signed-off-by: Max Brain <max.brain@ksoc.com>

---
+++ scripts/push_images.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo "Retagging and pushing images to $1"
+
+
+if [ -z "$PLUGIN_NAME" ]; then
+    echo "$PLUGIN_NAME must be defined"
+    exit 2;
+fi
+
+
+uniqueRepoName=$(docker image list --format "{{.Repository}}:{{.Tag}}" | grep "ksoc-guard" | cut -d'/' -f1 | sort | uniq | head -n 2)
+images=$(docker image list --format  "{{.Repository}}:{{.Tag}}" | grep "$PLUGIN_NAME" | grep "$uniqueRepoName")
+
+for line in $images; do
+    registry_id=$(echo "$line" | cut -d '.' -f 1)
+    repository_name_with_tag=$(echo "$line" | cut -d '/' -f 2-)
+    repository_name=$(echo "$repository_name_with_tag" | cut -d ':' -f 1)
+    repository=$(echo "$repository_name" | cut -d '/' -f 2)
+    tag=$(echo "$repository_name_with_tag" | cut -d ':' -f 2)
+
+    echo docker tag $line $1/$repository:$tag
+    echo docker push $1/$repository:$tag
+
+    matchingImages+=" $1/$repository:$tag"
+
+done
+
+manifestName=$(echo $matchingImages | cut -d ' ' -f 1 | sed 's/-[^-]*$//')
+
+echo "docker manifest create $manifestName $matchingImages"
+echo "docker manifest push $manifestName"
